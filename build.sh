24e23f50c0687eb57af353fed5aad7a90873d388
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-03-21 00:58:42 -0700

    Add build instructions in comments [skip ci]

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# git clone https://github.dev/sounddrill31/FOSSUnited-slides-appimages; cd FOSSUnited-slides-appimages; bash build.sh
+# Output will be in build/ folder
+
 echo "Fetching Kroki.io plugins"
 curl https://gist.githubusercontent.com/yhatt/8931dd98769bcc1c5b4b92fd1234a190/raw/94839633ae9665f688425843cbcb3e8778d2b848/marp.config.js -o build/marp.config.js
 curl https://gist.githubusercontent.com/yhatt/8931dd98769bcc1c5b4b92fd1234a190/raw/94839633ae9665f688425843cbcb3e8778d2b848/kroki-plugin.js -o build/kroki-plugin.js
